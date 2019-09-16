3381f1be31d8e010ce9289240b275a8649aa0f07
liuwenwen163 <liuwenwen163@gamil.com>
2019-09-16 21:23:37 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
-from flask import Flask
+from flask import Flask, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to My WatchList"
+    return "<h1>Hello Totoro</h1><img src='http://helloflask.com/totoro.gif'>"

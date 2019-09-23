8081d406f9f92e6197ba4d2a1d9cbceea54badd2
liuwenwen163 <liuwenwen163@gamil.com>
2019-09-23 16:17:52 +0800

    Add unit test with unittest.

--- watchlist.py
+++ watchlist.py
@@ -185,36 +185,44 @@ class Movie(db.Model):  # 表名movie
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
 
-# # 添加一些虚拟数据，引入click，需要在命令行敲击命令进行创建
-# @app.cli.command()
-# def forge():
-#     """Generate fake data,在命令行同级目录下，输入 flask forge 进行调用"""
-#     db.create_all()
-#
-#     name = 'Sen Dou'
-#     movies = [
-#         {'title': 'My Neighbor Totoro', 'year': '1988'},
-#         {'title': 'Dead Poets Society', 'year': '1989'},
-#         {'title': 'A Perfect World', 'year': '1993'},
-#         {'title': 'Leon', 'year': '1994'},
-#         {'title': 'Mahjong', 'year': '1996'},
-#         {'title': 'Swallowtail Butterfly', 'year': '1996'},
-#         {'title': 'King of Comedy', 'year': '1999'},
-#         {'title': 'Devils on the Doorstep', 'year': '1999'},
-#         {'title': 'WALL-E', 'year': '2008'},
-#         {'title': 'The Pork of Music', 'year': '2012'},
-#
-#     ]
-#
-#     user = User(name=name)
-#     db.session.add(user)
-#     for m in movies:
-#         movie = Movie(title=m['title'], year=m['year'])
-#         db.session.add(movie)
-#
-#     db.session.commit()
-#     click.echo("Done.")
+# 添加一些虚拟数据，引入click，需要在命令行敲击命令进行创建
+@app.cli.command()
+def forge():
+    """Generate fake data,在命令行同级目录下，输入 flask forge 进行调用"""
+    db.create_all()
+
+    name = 'Sen Dou'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo("Done.")
 
 
 # 编写命令创建管理员账号
